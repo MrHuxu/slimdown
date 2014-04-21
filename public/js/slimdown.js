6613f5a0a353e7c952acd21f4823aaba22fb7ca7
MrHuxu <hxtheone@gmail.com>
2014-04-21 16:13:57 +0800

    test disqus

--- public/js/slimdown.js
+++ public/js/slimdown.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+  /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+  var disqus_shortname = 'guaimao'; // required: replace example with your forum shortname
+
+  /* * * DON'T EDIT BELOW THIS LINE * * */
+  (function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  })();
+
   var obj
   $('#index>div').hover(function(){
     $(this).animate({ 
